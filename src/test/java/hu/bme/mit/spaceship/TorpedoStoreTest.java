7d2ed0d267e937041fa3e6409e109a6d357c5b42
lichterberci <lichterberci@gmail.com>
2023-10-25 15:32:16 +0200

    Added a bunch of tests

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,20 +1,50 @@
 package hu.bme.mit.spaceship;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TorpedoStoreTest {
 
-  @Test
-  void fire_Success(){
-    // Arrange
-    TorpedoStore store = new TorpedoStore(1);
+	@Test
+	void fire_Success(){
+		// Arrange
+		TorpedoStore store = new TorpedoStore(1);
 
-    // Act
-    boolean result = store.fire(1);
+		// Act
+		boolean result = store.fire(1);
 
-    // Assert
-    assertEquals(true, result);
-  }
+		// Assert
+		assertTrue(result);
+		assertEquals(0, store.getTorpedoCount());
+	}
+
+	@Test
+	void cannotFireEmpty () {
+
+		TorpedoStore store = new TorpedoStore(0);
+
+		// Act
+		assertThrows(IllegalArgumentException.class, () -> store.fire(1));
+	}
+
+	@Test
+	void cannotFireMore () {
+
+		TorpedoStore store = new TorpedoStore(10);
+
+		assertThrows(IllegalArgumentException.class, () -> store.fire(  11));
+	}
+
+	@Test
+	void fails () {
+
+		TorpedoStore store = new TorpedoStore(10, 1);
+
+		// Act
+		boolean result = store.fire(1);
+
+		// Assert
+		assertFalse(result);
+	}
 }
